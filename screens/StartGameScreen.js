b1ea59dfc1b3e7624bb075515ebf1a93b25bec34
Marco Bassaletti <bassaletti@gmail.com>
2021-03-31 16:05:10 -0300

    Card container shows selected number after confirm.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {View, Text, Button, TouchableWithoutFeedback, StyleSheet, Keyboard, Alert} from 'react-native';
 import Card from "../components/Card";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
@@ -37,7 +38,13 @@ const StartGameScreen = props => {
 
   let confirmedOutput = null;
   if (confirmed) {
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="Start Game" />
+      </Card>
+    );
   }
 
   return (
@@ -87,6 +94,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: 'center',
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   }
 });
 
