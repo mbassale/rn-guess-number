71a8649243e5d3a3ae1125430eee3a32a038a6e9
Marco Bassaletti <bassaletti@gmail.com>
2021-03-31 15:51:40 -0300

    Show an alert when the number is invalid.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, Button, TouchableWithoutFeedback, StyleSheet, Keyboard} from 'react-native';
+import {View, Text, Button, TouchableWithoutFeedback, StyleSheet, Keyboard, Alert} from 'react-native';
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Colors from '../constants/colors';
@@ -22,6 +22,11 @@ const StartGameScreen = props => {
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert('Invalid number!', 'Number has to be a number beetween 1 and 99', [{
+        text: 'Okay',
+        style: 'destructive',
+        onPress: resetInputHandler
+      }]);
       return;
     }
     setConfirmed(true);
